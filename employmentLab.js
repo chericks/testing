03cb432258251ec71960e3f6882de6e0de79d3e1
Erickson <erichrc@dunwoody.edu>
2017-09-21 20:53:26 -0500

    adding html and js

---
+++ employmentLab.js
@@ -0,0 +1,170 @@
+var employee = [
+  {
+    "name": "Christopher Ercikson",
+    "age": 33,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Sarah Brown",
+    "age": 30,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Gayle Erickson",
+    "age": 68,
+    "marital": "single",
+    "employment": "retired",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Jordan Rincon",
+    "age": 32,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "St. Paul"
+  },
+  {
+    "name": "Trevor Lannister",
+    "age": 29,
+    "marital": "single",
+    "employment": "part-time",
+    "city": "Norwood"
+  },
+  {
+    "name": "Deb Dartcsh",
+    "age": 65,
+    "marital": "married",
+    "employment": "retired",
+    "city": "Stillwater"
+  },
+  {
+    "name": "Meaghan Hanson",
+    "age": 30,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "River Falls"
+  },
+  {
+    "name": "Noah Waiwaiole",
+    "age": 28,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Moorehead"
+  },
+  {
+    "name": "Greg Merk",
+    "age": 60,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "St louis park"
+  },
+  {
+    "name": "Andrew Lillion",
+    "age": 32,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "St. Paul"
+  },
+  {
+    "name": "James Madick",
+    "age": 46,
+    "marital": "married",
+    "employment": "part-time",
+    "city": "Minnetonka"
+  },
+  {
+    "name": "Calvin Stine",
+    "age": 23,
+    "marital": "married",
+    "employment": "part-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Hobbes Seville",
+    "age": 34,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Sonya Deerfield",
+    "age": 48,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "St. Paul"
+  },
+  {
+    "name": "Lenny Manchester",
+    "age": 32,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "Bloomington"
+  },
+  {
+    "name": "Rick Sanchez",
+    "age": 51,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Bloomington"
+  },
+  {
+    "name": "Morty Weber",
+    "age": 20,
+    "marital": "single",
+    "employment": "part-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Sean Donkers",
+    "age": 33,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "St. Paul"
+  },
+  {
+    "name": "Eddie Lacrosse",
+    "age": 42,
+    "marital": "single",
+    "employment": "part-time",
+    "city": "Minnetonka"
+  },
+  {
+    "name": "Stephanie Guissenger",
+    "age": 69,
+    "marital": "married",
+    "employment": "retired",
+    "city": "Minnetonka"
+  },
+  {
+    "name": "Keira Hamilton",
+    "age": 39,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "Prior Lake"
+  },
+  {
+    "name": "Wade Butterbean",
+    "age": 36,
+    "marital": "single",
+    "employment": "part-time",
+    "city": "Minneapolis"
+  },
+  {
+    "name": "Annie McGinnie",
+    "age": 24,
+    "marital": "single",
+    "employment": "full-time",
+    "city": "Richfield"
+  },
+  {
+    "name": "Kent Jensen",
+    "age": 33,
+    "marital": "married",
+    "employment": "full-time",
+    "city": "Minneapolis"
+  }
+]
